07241a2239e7c39753d5cf0015d7ee4b48fba8af
ABSanthosh <a.b.santhosh02@gmail.com>
2024-06-01 17:46:33 +0530

    chore: remove all files

--- scripts/on-mesa-update.sh
+++
@@ -1,7 +0,0 @@
-# https://discourse.nixos.org/t/chromium-blank-pages/30281/4
-# https://community.brave.com/t/latest-brave-update-fails-to-render-any-webpage-correctly/485593
-# https://github.com/electron/electron/issues/40366
-
-# Sometimes when mesa drivers are updated, chromium based browsers will not render any webpages
-# and electron apps will not render properly. This script will clear the gpu cache and fix the issue.
-find ~/.config -type d -name 'GPUCache' -exec rm -rf {} +
--- scripts/screenshot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-TMPFILE=`mktemp -u /tmp/screenshotclip.XXXXXXXX.png`
-gnome-screenshot -af $TMPFILE && xclip $TMPFILE -selection clipboard -target image/png; rm $TMPFILE || echo ""

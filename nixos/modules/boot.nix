4f9020685a77d1dc060848f0d79c0a923879bd6e
ABSanthosh <a.b.santhosh02@gmail.com>
2024-07-03 01:03:46 +0530

    chore: modular boot

---
+++ nixos/modules/boot.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ...}:
+{
+	boot = {
+		loader = {
+			systemd-boot.enable = true;
+			efi.canTouchVariables = true;
+			timeout = 0;
+		};
+		tmp.cleanOnBoot = true;
+		supportedFilesystems = [ "ntfs" "exfat" ];
+		kernalParams = [ "quite" "splash" "loglevel=0" ];
+		initrd = {
+			verbose = false;
+		};
+		consoleLogLevel = 0;
+		plymouth.enable = true;
+	};
+
+	systemd = {
+		watchdog.rebootTime = "0";
+		targets.network-online.wantedBy = pkgs.lib.mkForce [ ]; # Normally ["multi-user.target"]
+		services = {
+			systemd-udev-settle.enable = false;
+			NetworkManager-wait-online = {
+				enable = false;
+				wantedBy = pkgs.lib.mkForce [ ]; # Normally ["network-online.target"]
+			};
+		};
+	};
+}

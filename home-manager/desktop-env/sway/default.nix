34a86437deaf30c1fed6a42de462ef6a395031e6
ABSanthosh <a.b.santhosh02@gmail.com>
2024-07-04 18:32:09 +0530

    feat: Update Sway configuration to improve media control and window layout

--- home-manager/desktop-env/sway/default.nix
+++ home-manager/desktop-env/sway/default.nix
@@ -70,10 +70,63 @@ in
       bindsym XF86AudioLowerVolume  exec 'wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-
       bindsym XF86AudioMute         exec 'wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle'
 
+      # Media  
+      bindsym XF86AudioPrev exec playerctl previous
+      bindsym XF86AudioPlay exec playerctl play-pause
+      bindsym XF86AudioNext exec playerctl next
+
+      bindsym F9 exec playerctl previous
+      bindsym F10 exec playerctl play-pause
+      bindsym F11 exec playerctl next
+
       # Borders
       default_border none 
       # for_window [title="^.*"] title_format " "
-      # default_floating_border normal 0
+      default_floating_border normal 0
+
+      for_window [title="Picture-in-picture"] {
+        floating enable
+        sticky enable
+        resize set width 100ppt
+        resize set width 340px
+      }
+
+      for_window [title="Open Files"] {
+        floating enable 
+        resize set width 800px height 600px
+      }
+      for_window [title="Open Folders"] {
+        floating enable 
+        resize set width 800px height 600px
+      }
+      for_window [title="Open File"] {
+        floating enable 
+        resize set width 800px height 600px
+      }
+      for_window [title="Open Folder"] {
+        floating enable 
+        resize set width 800px height 600px
+      }
+      for_window [title="Settings"] {
+        floating enable 
+        resize set width 800px height 600px
+      }
+      for_window [title="Save File"] {
+        floating enable 
+        resize set width 800px height 600px
+      }
+      for_window [title="Copy Files"] {
+        floating enable 
+        resize set width 800px height 600px
+      }
+      for_window [title="Choose Files"] {
+        floating enable 
+        resize set width 800px height 600px
+      }
+      for_window [title="File Properties"] {
+        floating enable 
+        resize set width 800px height 600px
+      }
 
       # Tap to click
       input "type:touchpad" {
@@ -83,8 +136,15 @@ in
         middle_emulation enabled
       }
 
+      # Set wallpaper
       output "*" bg '/etc/a_rocket_launching_in_the_sky.png' fill
+
+      # Disable HDMI-4
+      output HDMI-A-4 disable
       
+      # set window layout as tabbed by default
+      workspace_layout tabbed
+
       exec dbus-sway-environment
       exec configure-gtk
     '';
@@ -109,6 +169,7 @@ in
 
       glib
       bemenu
+      playerctl
       configure-gtk
       dbus-sway-environment
     ];

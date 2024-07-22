396b8367858d4a02b3a71ed76dc329bc40055a7a
ABSanthosh <a.b.santhosh02@gmail.com>
2024-07-22 16:33:46 +0530

    chore: update nix packages

--- nixos/modules/aliases.nix
+++ nixos/modules/aliases.nix
@@ -5,5 +5,6 @@
     nix-clear = "sudo nix profile wipe-history --profile /nix/var/nix/profile/system --older-than 5d && sudo nixos-rebuild switch";
     # https://github.com/Misterio77/nix-starter-configs#usage
     nix-refresh = "sudo nixos-rebuild switch --flake .#zoro";
+    nix-update = "nix flake update";
   };
 }

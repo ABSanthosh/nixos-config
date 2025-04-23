49cf06be706e3ec7dab5e43dc03f4b5fbc8a1663
ABSanthosh <a.b.santhosh02@gmail.com>
2025-04-23 18:59:40 -0400

    feat: add anime-cplusplus.jpg as the new default wallpaper and update configuration to reflect the change

--- variables.nix
+++ variables.nix
@@ -16,6 +16,6 @@
   # Program Options
   browser = "brave"; # Set Default Browser (google-chrome-stable for google-chrome)
   terminal = "kitty"; # Set Default System Terminal
-  wallpaper = assets/Wallpapers/Misc/anime-cplusplus.png;
+  wallpaper = assets/Wallpapers/Misc/anime-cplusplus.jpg;
   stateVersion = "24.05";
 }
